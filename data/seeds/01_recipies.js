2d447f96ffeb0eae050f4d3f8927d503433f7551
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-14 17:19:57 -0500

    getting seeded

--- data/seeds/01_recipies.js
+++ data/seeds/01_recipies.js
@@ -1,11 +1,23 @@
+const faker = require("faker")
 
-exports.seed = function(knex) {
+exports.seed = async function(knex, Promise) {
   // Deletes ALL existing entries
 
 let dataToSeed = []
-for (let i=1;i<100;i++){
+for (let i=1;i<1;i++){
   dataToSeed.push({"id":i, name:`value ${i}`})
 }
+
+// let createRecipies = () =>{
+//   name: faker.image.food
+// }
+// const fakeRecipe = []
+// for (let i=1;i<100;i++){
+// fakeRecipe.push(createRecipies)
+// }
+
+// await knex("recipies").insert(fakeRecipe)
+
 // console.log(dataToSeed)
 
   return knex('recipies').del()
