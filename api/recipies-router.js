71b0bd32039ca7016ee495b6722daf6d850461cc
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-14 17:39:53 -0500

    getting model helpers done

--- api/recipies-router.js
+++ api/recipies-router.js
@@ -1,9 +1,11 @@
 const server = require('express').Router();
 const db = require('../data/data-config');
+const schemes = require("./recipies-model")
 
 server.get('/', (req, res) => {
   // get all recipies from the database
-  db('recipies')
+  // db('recipies')
+  schemes.find()
   .then(recipies => {
     res.status(200).json(recipies);
   })
