2f6bcf32ea66ff6c1c5f902711a492cf4a50ddc9
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-14 19:41:00 -0500

    got getshoppinglist made

--- data/seeds/03_ingredients.js
+++ data/seeds/03_ingredients.js
@@ -5,7 +5,19 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('ingredients').insert([
-        {id: 1, name: 'Salmon'}
+        {id: 1, name: 'Salmon'},
+        {id: 2, name: 'Rice'},
+        {id: 3, name: 'Broth'},
+        {id: 4, name: 'Golden Grahams'},
+        {id: 5, name: 'Maple Syrup'},
+        {id: 7, name: 'Olive Oil'},
+        {id: 8, name: 'Butter'},
+        {id: 9, name: 'Salt'},
+        {id: 10, name: 'Herbs du Provence'},
+        {id: 11, name: 'Milk'},
+        {id: 12, name: 'Chicken'},
+
+
       ]);
     });
 };
--- data/seeds/04_steps.js
+++ data/seeds/04_steps.js
@@ -5,7 +5,16 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('steps').insert([
-        {id: 1, step_number: 1, instructions: 'Cook it', recipe_id: 1, ingredient_id:1},
+        {id: 1, step_number: 1, instructions: 'Pour Cereal', recipe_id: 1, ingredient_id:4},
+        {id: 2, step_number: 1, instructions: 'Pour Milk', recipe_id: 1, ingredient_id:11},
+        {id: 3, step_number: 1, instructions: 'Cook Rice', recipe_id: 2, ingredient_id:2},
+        {id: 4, step_number: 1, instructions: 'Cook Chicken', recipe_id: 2, ingredient_id:12},
+        {id: 5, step_number: 1, instructions: 'Mix with Herbs', recipe_id: 2, ingredient_id:10},
+        {id: 6, step_number: 1, instructions: 'Dress Salmon', recipe_id: 3, ingredient_id:1},
+        {id: 7, step_number: 1, instructions: 'Glaze with Maple Syrup', recipe_id: 3, ingredient_id:5},
+        {id: 8, step_number: 1, instructions: 'Add Salt', recipe_id: 3, ingredient_id:9},
+        {id: 9, step_number: 1, instructions: 'Bake Item', recipe_id: 3, ingredient_id:9}
+
 
       ]);
     });
